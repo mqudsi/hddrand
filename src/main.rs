34ad31cd048d9ee20e6f8d3330717f971cda5a01
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2023-01-14 21:07:03 -0600

    Add support for Windows
    Under Windows, a disk is specified with a global namespace prefix such as
    `\??\PhysicalDrive7`
    
    The drive number may be obtained by inspecting drive names shown in
    `diskmgmt.msc`.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,28 @@ use std::env;
 use std::fs::OpenOptions;
 use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::Path;
-use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::sync::Arc;
 use std::time::{Duration, Instant};
 
+const ENOENT: i32 = 2;
 const ENOSPC: i32 = 28;
+#[cfg_attr(not(windows), allow(dead_code))]
+const ERROR_PATH_NOT_FOUND: i32 = 3;
+#[cfg_attr(not(windows), allow(dead_code))]
+const ERROR_ACCESS_DENIED: i32 = 5;
+
+fn print_usage() {
+    #[cfg(not(windows))]
+    println!("USAGE: hddrand [--verify] /dev/disk");
+    #[cfg(windows)]
+    println!("USAGE: hddrand [--verify] \\??\\PhysicalDrive8");
+}
 
 fn main() {
     let args: Vec<_> = env::args().skip(1).collect();
     if args.len() == 0 {
-        println!("USAGE: hddrand [--verify] /dev/disk");
+        print_usage();
         std::process::exit(0);
     }
 
@@ -23,7 +35,7 @@ fn main() {
         match arg.as_str() {
             "verify" | "--verify" => verify = true,
             path => {
-                if path.starts_with('/') {
+                if path.starts_with('/') || path.starts_with('\\') {
                     drive = Some(path);
                 }
             }
@@ -32,16 +44,35 @@ fn main() {
 
     let drive = match drive {
         None => {
-            println!("USAGE: hddrand [--verify] /dev/disk");
+            print_usage();
             std::process::exit(1);
         }
         Some(drive) => drive,
     };
 
     let path = Path::new(&drive);
-    if !path.exists() {
+    // NtCreateFile expects a path in the NT namespace (\) while CreateFile takes paths from the
+    // global namespace \GLOBAL?? (aka \??). PhysicalDriveX is in the global namespace but not in
+    // the root of the NT namespace. We can open PhysicalDriveX by prepending \\?\ to the path, but
+    // Path::exists() doesn't support such a path, so we have to use our own exists wrapper.
+    #[cfg_attr(windows, allow(unreachable_code))]
+    while !path.exists() {
+        #[cfg(windows)]
+        match nt_exists(&drive) {
+            NtFileStatus::Found => {
+                break;
+            }
+            NtFileStatus::NotFound => {
+                eprintln!("{}: not found!", path.display());
+                std::process::exit(ERROR_PATH_NOT_FOUND);
+            }
+            NtFileStatus::AccessDenied => {
+                eprintln!("{}: access denied!", path.display());
+                std::process::exit(ERROR_ACCESS_DENIED);
+            }
+        }
         eprintln!("{}: not found!", path.display());
-        std::process::exit(2); // ENOENT
+        std::process::exit(ENOENT);
     }
 
     let result = if verify {
@@ -227,4 +258,69 @@ fn start_progress_thread(total_bytes: Arc<AtomicUsize>, done: Arc<AtomicBool>) {
             }
         }
     });
+}
+
+#[cfg(windows)]
+enum NtFileStatus {
+    NotFound,
+    Found,
+    AccessDenied,
+}
+
+#[cfg(windows)]
+fn nt_exists(path: &str) -> NtFileStatus {
+    use windows_sys::{
+        Win32::Foundation::*, Win32::Storage::FileSystem::*, Win32::System::Kernel::*,
+        Win32::System::WindowsProgramming::*,
+    };
+
+    let mut unicode_str: Vec<u16> = path.encode_utf16().into_iter().collect();
+    let mut unicode_path = UNICODE_STRING {
+        Length: (unicode_str.len() * std::mem::size_of::<u16>()) as _,
+        MaximumLength: (unicode_str.len() * std::mem::size_of::<u16>()) as _,
+        Buffer: unicode_str.as_mut_ptr(),
+    };
+    let mut obj_attr = OBJECT_ATTRIBUTES {
+        Length: 0,
+        RootDirectory: 0,
+        ObjectName: &mut unicode_path as *mut _,
+        Attributes: OBJ_CASE_INSENSITIVE as _,
+        SecurityDescriptor: std::ptr::null_mut() as *mut _,
+        SecurityQualityOfService: std::ptr::null_mut() as *mut _,
+    };
+    obj_attr.Length = std::mem::size_of_val(&obj_attr) as _;
+
+    unsafe {
+        let mut handle = 0;
+        let mut status_block: IO_STATUS_BLOCK = std::mem::zeroed();
+
+        let result = NtCreateFile(
+            &mut handle as *mut _,
+            FILE_READ_DATA | FILE_READ_ATTRIBUTES,
+            &mut obj_attr as *mut _,
+            &mut status_block as *mut _,
+            std::ptr::null_mut() as *mut _,
+            FILE_ATTRIBUTE_NORMAL,
+            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+            FILE_OPEN,
+            FILE_NON_DIRECTORY_FILE,
+            std::ptr::null::<core::ffi::c_void>() as *mut _,
+            0,
+        );
+
+        /*
+        eprintln!("NTSTATUS: {:#X}", result);
+        // Information is FILE_CREATED/OPENED/OVERWRITTEN/SUPERSEDED/EXISTS/DOES_NOT_EXIST
+        eprintln!("IO_STATUS_BLOCK.Information: {}", status_block.Information);
+        */
+
+        match result {
+            STATUS_SUCCESS => {
+                NtClose(handle);
+                NtFileStatus::Found
+            }
+            STATUS_ACCESS_DENIED => NtFileStatus::AccessDenied,
+            _ => NtFileStatus::NotFound,
+        }
+    }
 }
